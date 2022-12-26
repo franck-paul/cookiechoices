4185d9124d2a89e978b0167a95549d19efbb3461
franck <carnet.franck.paul@gmail.com>
2022-12-26 18:03:51 +0100

    New install

--- _install.php
+++ _install.php
@@ -14,21 +14,15 @@ if (!defined('DC_CONTEXT_ADMIN')) {
     return;
 }
 
-$this_version      = dcCore::app()->plugins->moduleInfo('cookiechoices', 'version');
-$installed_version = dcCore::app()->getVersion('cookiechoices');
-
-if (version_compare((string) $installed_version, $this_version, '>=')) {
+if (!dcCore::app()->newVersion(basename(__DIR__), dcCore::app()->plugins->moduleInfo(basename(__DIR__), 'version'))) {
     return;
 }
 
-dcCore::app()->blog->settings->addNamespace('cookiechoices');
 dcCore::app()->blog->settings->cookiechoices->put('message', 'By using our services, you agree to our use of cookies.', 'string', 'Visitor message', true, true);
 dcCore::app()->blog->settings->cookiechoices->put('close', 'Got it', 'string', 'Close message', true, true);
 dcCore::app()->blog->settings->cookiechoices->put('learnmore', 'Learn more', 'string', 'Learn more message', true, true);
 dcCore::app()->blog->settings->cookiechoices->put('url', 'https://www.cookiechoices.org/', 'string', 'Learn more URL', true, true);
 dcCore::app()->blog->settings->cookiechoices->put('appearance', 2, 'integer', 'Message appearance', true, true);
 dcCore::app()->blog->settings->cookiechoices->put('anywhere', false, 'boolean', 'Display message on every page', true, true);
-
-dcCore::app()->setVersion('cookiechoices', $this_version);
 
 return true;
