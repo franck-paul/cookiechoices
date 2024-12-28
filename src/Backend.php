e002fcc17c9875643162617d5f16a622ad3a3b09
franck <carnet.franck.paul@gmail.com>
2024-12-28 06:55:40 +0100

    PHP code review (Rector/PHPStan)

--- src/Backend.php
+++ src/Backend.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief cookiechoices, a plugin for Dotclear 2
  *
@@ -22,7 +23,8 @@ class Backend extends Process
     public static function init(): bool
     {
         // dead but useful code, in order to have translations
-        __('cookiechoices') . __('Cookie Consent System');
+        __('cookiechoices');
+        __('Cookie Consent System');
 
         return self::status(My::checkContext(My::BACKEND));
     }
