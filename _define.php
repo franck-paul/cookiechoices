1830fa5f4e4c849f3c04463934890310f9d5c799
franck <contact@open-time.net>
2026-05-06 17:30:47 +0200

    New release 6.2

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'cookiechoices',
     'Cookie Consent System',
     'Franck Paul',
-    '6.1',
+    '6.2',
     [
-        'date'        => '2026-04-10T16:08:39+0200',
+        'date'        => '2026-05-06T17:30:47+0200',
         'requires'    => [['core', '2.36']],
         'permissions' => 'My',
         'type'        => 'plugin',
