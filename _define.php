0c713705a0cd18d2f9d8d3394a6d1e43dcae33ae
franck <contact@open-time.net>
2026-08-13 16:38:44 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,23 +11,27 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'cookiechoices',
-    'Cookie Consent System',
-    'Franck Paul',
-    '7.0',
-    [
-        'date'        => '2026-08-03T09:49:37+0200',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'settings'    => [
-            'blog' => '#params.cookiechoices',
-        ],
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=cookiechoices',
-        'support'    => 'https://github.com/franck-paul/cookiechoices',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/cookiechoices/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'cookiechoices',
+        'Cookie Consent System',
+        'Franck Paul',
+        '7.0',
+        [
+            'date'        => '2026-08-03T09:49:37+0200',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'settings'    => [
+                'blog' => '#params.cookiechoices',
+            ],
+
+            'details'    => 'https://open-time.net/?q=cookiechoices',
+            'support'    => 'https://github.com/franck-paul/cookiechoices',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/cookiechoices/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
